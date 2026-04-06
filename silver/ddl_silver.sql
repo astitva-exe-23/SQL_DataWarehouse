e3988975188f4aadc14a690a3cf6df5915055c0b
Astitva Singh <110301422+astitva-exe-23@users.noreply.github.com>
2026-04-06 13:05:37 +0530

    Create SQL tables for silver data warehouse

---
+++ silver/ddl_silver.sql
@@ -0,0 +1,77 @@
+USE data_warehouse;
+
+
+DROP TABLE IF EXISTS silver_crm_cust_info;
+CREATE TABLE silver_crm_cust_info(
+cst_id	INT,
+cst_key	NVARCHAR(50),
+cst_firstname	NVARCHAR(50),
+cst_lastname	NVARCHAR(50),
+cst_marital_status	NVARCHAR(50),
+cst_gndr	NVARCHAR(50),
+cst_create_date DATE,
+dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+DROP TABLE IF EXISTS silver_crm_prd_info;
+CREATE TABLE silver_crm_prd_info(
+prd_id	INT,
+prd_key	NVARCHAR(50),
+prd_nm	NVARCHAR(50),
+prd_cost NVARCHAR(50),
+prd_line	NVARCHAR(50),
+prd_start_dt	DATETIME,
+prd_end_dt   DATETIME,
+dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+DROP TABLE IF EXISTS silver_crm_sales_details;
+CREATE TABLE silver_crm_sales_details(
+sls_ord_num	NVARCHAR(50),
+sls_prd_key	NVARCHAR(50),
+sls_cust_id	INT,
+sls_order_dt INT,
+sls_ship_dt	INT,
+sls_due_dt	INT,
+sls_sales	INT,
+sls_quantity INT,
+sls_price  INT,
+dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+DROP TABLE IF EXISTS silver_crm_erp_custaz12;
+CREATE TABLE silver_erp_custaz12(
+cid NVARCHAR(50),
+bdate DATE,
+cntry NVARCHAR(50),
+dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+DROP TABLE IF EXISTS silver_erp_loc;
+CREATE TABLE silver_erp_loc(
+cid	nvarchar(50),
+cntry NVARCHAR(50),
+dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+DROP TABLE IF EXISTS silver_erp_px_cat_g1v2;	
+CREATE TABLE silver_erp_px_cat_g1v2(
+id	  NVARCHAR(50),
+cat  NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50),
+dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+
+
+
+
